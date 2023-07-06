e2397da4dd311aafeadac75e1ed9b4dcc7ab8505
RossiGu <gustavo.2017316329@gmail.com>
2023-07-06 16:13:42 -0300

    LandingPageFacul

--- landingpage/src/pages/Home/Home.js
+++ landingpage/src/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Home.css'
+import image from '../../images/homeImage.png'
 
 const Home = () => {
   return (
-    <div>
-        <h2>Home</h2>
+    <div className='home'>
+      <div className='content'>
+        <h2>Construa um futuro melhor</h2>
+        <p>Descubra maneiras simples e práticas de preservar o meio ambiente e contribuir para um futuro sustentável.</p>
+        <button>Junte-se agora!</button>
+      </div>
+      <div className='contentImage'>
+        <img src={image} alt="Imagem" />
+      </div>
     </div>
   )
 }
