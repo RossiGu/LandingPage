736874957d60b48becb0af321de6baf441a5c324
RossiGu <gustavo.2017316329@gmail.com>
2023-07-15 14:36:55 -0300

    LandingPageFacul

--- landingpage/src/pages/Preservacao/Preservacao.js
+++ landingpage/src/pages/Preservacao/Preservacao.js
@@ -6,42 +6,61 @@ import reciclagem from '../../images/imagesPage/reciclagem.png'
 import arvore from '../../images/imagesPage/arvore.png'
 import transporte from '../../images/imagesPage/transporte.png'
 import rios from '../../images/imagesPage/rios.png'
+import { Link } from 'react-router-dom'
 
 const Preservacao = () => {
   return (
-    <div className='contentP'>
-      <div className='title'>
-        <h2>Algumas maneiras de preservar o nosso meio ambiente!</h2>
-      </div>
-      <div className='style'>
-        <img src={eco} alt="" />
-        <h2>Conservação de ecossistemas<br /><p>Veja mais</p></h2>
-      </div>
-      <div className='style'>
-        <img src={recursos} alt="" />
-        <h2>Equilíbrio de recursos naturais<br /><p>Veja mais</p></h2>
-      </div>
-      <div className='style'>
-        <img src={poluicao} alt="" />
-        <h2>Combate a poluição<br /><p>Veja mais</p></h2>
-      </div>
-      <div className='style'>
-        <img src={reciclagem} alt="" />
-        <h2>Reciclagem<br /><p>Veja mais</p></h2>
-      </div>
-      <div className='style'>
-        <img src={arvore} alt="" />
-        <h2>Plantar árvores<br /><p>Veja mais</p></h2>
-      </div>
-      <div className='style'>
-        <img src={transporte} alt="" />
-        <h2>Transportes sustentáveis<br /><p>Veja mais</p></h2>
-      </div>
-      <div className='style'>
-        <img src={rios} alt="" />
-        <h2>Limpeza dos corpos hídricos<br /><p>Veja mais</p></h2>
-      </div>
+
+  <div className='contentP'>
+    <div className='title'>
+      <h2>Algumas maneiras de preservar o nosso meio ambiente!</h2>
     </div>
+      <div className='contentStyle'>
+        <div className='style'>
+          <img src={eco} alt="" />
+          <Link to='/'>
+            <h2>Conservação de ecossistemas<br /></h2>
+          </Link>
+        </div>
+        <div className='style'>
+          <img src={recursos} alt="" />
+          <Link to='/'>
+            <h2>Equilíbrio de recursos naturais<br /></h2>
+          </Link>
+        </div>
+        <div className='style'>
+          <img src={poluicao} alt="" />
+          <Link to='/'>
+            <h2>Combate a poluição<br /></h2>
+          </Link>
+        </div>
+        <div className='style'>
+          <img src={reciclagem} alt="" />
+          <Link to='/'>
+            <h2>Reciclagem<br /></h2>
+          </Link>
+        </div>
+        <div className='style'>
+          <img src={arvore} alt="" />
+          <Link to='/'>
+            <h2>Plantar árvores<br /></h2>
+          </Link>
+        </div>
+        <div className='style'>
+          <img src={transporte} alt="" />
+          <Link to='/'>
+            <h2>Transportes sustentáveis<br /></h2>
+          </Link>
+        </div>
+        <div className='style'>
+          <img src={rios} alt="" />
+          <Link to='/'>
+            <h2>Limpeza dos corpos hídricos<br /></h2>
+          </Link>
+        </div>
+      </div>
+  </div>
+    
   )
 }
 
